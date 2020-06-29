dc905c713994566fb5123b4e71f13c20f9f2519d
nguyen <nguyen@th-brandenburg.de>
2020-06-29 10:45:15 +0200

    Normalized heights

--- config/config.fetch.php
+++ config/config.fetch.php
@@ -1,24 +1,52 @@
 <?php
 
+// Normalize values to the same given maximum
+function normalize($min, $max, $wert){
+    return ($wert - $min) / $max;
+}
+
+$i = -25;
+$j = 0;
+$amountValues = 50;
+
 // Query
-$sql = "SELECT Revenue FROM f_gbi LIMIT 50";
+$sql = "SELECT Revenue FROM f_gbi LIMIT $amountValues";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
 
-    $i = -25;
     // Fetch results
     while($row = $result->fetch_assoc()) {
 
-        $dummy = $row['Revenue'];
-        $dummy = $dummy * 0.0005;
+        // Write rows from table into array
+        $array[$j] = $row['Revenue'];;
+        $j++;
 
-        $pos = $dummy / 2;
-
-        $i = $i + 1;
-        echo '<a-box color="#c0ebf0" depth="0.5" height="'.$dummy.'" width="0.5" position="'.$i.' '.$pos.' -20"></a-box>';
-        echo "<script>console.log(".$dummy.");</script>";
     }
+
+    // Get maximum value in fetched rows
+    $maxValue = max($array);
+
+    // Set minimum value to 0 for normalization
+    $minValue = 0;
+
+    // Loop over normalized values and display them
+    for ($k = 0; $k < $amountValues; $k++) {
+
+        // Call normalize function that can be adjusted by changing the factor
+        $normalizedValue = normalize($minValue, $maxValue, $array[$k]) * 15;
+
+        echo "<script>console.log(".$normalizedValue.");</script>";
+
+        // Center elements vertically
+        $pos = $normalizedValue / 2;
+        $i++;
+
+        echo '<a-box color="#c0ebf0" depth="0.5" height="'.$normalizedValue.'" width="0.5" position="'.$i.' '.$pos.' -20"></a-box>';
+        //echo '<a-sphere color=red radius="0.2" position="'.$i.' '.$normalizedValue.' -20"></a-sphere>';
+    
+    }
+
 
 } else {
 
--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,7 @@
 
     <head>
         <meta charset="utf-8">
-        <title>bt_2020</title>
+        <title>bt-2020</title>
 
         <script src="https://aframe.io/releases/1.0.4/aframe.min.js"></script>
     </head>
@@ -11,4 +11,4 @@
     <body>
     
         <a-scene background="color: #343d42">
-            <a-entity camera look-controls wasd-controls="acceleration:200" position="0 8 0"></a-entity>
+            <a-entity camera look-controls wasd-controls="acceleration:500" position="0 3 0"></a-entity>
